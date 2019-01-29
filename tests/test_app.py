9e2d3d3729c162fc39db292e1250ee8c537e92df
Alex McLarty <alex.mclarty@hanzoarchives.com>
2019-01-29 14:29:35 +0000

    Add test to support updating an endpoint.
    Co-Authored-By: Alexander Price <nullprice@users.noreply.github.com>

--- tests/test_app.py
+++ tests/test_app.py
@@ -76,6 +76,40 @@ class IntegrationTest(unittest.TestCase):
         response = self.app.get('/non_standard')
         assert response.status_code == 300
 
+    def test_update_endpoint(self):
+
+        # Setup endpoint
+        payload = {
+            "endpoint": "/endpoint_to_update",
+            "methods": ["GET"],
+            "status_code": 200,
+            "json_response": {'cheese_flavour': 'cheddar'}
+        }
+
+        json_payload = json.dumps(payload)
+
+        self.app.post('/register', data=json_payload, headers={'content-type': 'application/json'})
+
+        response = self.app.get('/endpoint_to_update')
+        assert response.json['cheese_flavour'] == 'cheddar'
+        assert response.status_code == 200
+
+        # Setup endpoint with a different response
+        payload = {
+            "endpoint": "/endpoint_to_update",
+            "methods": ["GET"],
+            "status_code": 200,
+            "json_response": {'cheese_flavour': 'gouda'}
+        }
+
+        json_payload = json.dumps(payload)
+
+        self.app.post('/register', data=json_payload, headers={'content-type': 'application/json'})
+
+        response = self.app.get('/endpoint_to_update')
+        assert response.status_code == 200
+        assert response.json['cheese_flavour'] == 'gouda'
+
     def test_get_code(self):
         response = self.app.get('/get_code')
         assert response.status_code == 200
